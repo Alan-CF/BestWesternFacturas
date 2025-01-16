584f53e7a1fb656aab40a4c084200160877f0def
Alan Canales <156135214+Alan-CF@users.noreply.github.com>
2025-01-16 18:21:01 +0000

    [ADD] processFile lee df y wb

--- file_processor.py
+++ file_processor.py
@@ -2,6 +2,7 @@ import zipfile
 import streamlit as st
 from io import BytesIO
 import pandas as pd
+import openpyxl
 
 
 class FileProcessor:
@@ -11,30 +12,31 @@ class FileProcessor:
         pass
     
     def processZip(self, zip_file):
-            try:
-                with zipfile.ZipFile(BytesIO(zip_file.read())) as zf:
-                    file_list = zf.namelist()
+        try:
+            with zipfile.ZipFile(BytesIO(zip_file.read())) as zf:
+                file_list = zf.namelist()
 
-                    for file_name in file_list:
-                        if file_name.endswith(('.xls', '.xlsx')):
-                            with zf.open(file_name) as file:
-                                try:
+                for file_name in file_list:
+                    if file_name.endswith(('.xls', '.xlsx')):
+                        with zf.open(file_name) as file:
+                            try:
 
 
-                                    df = pd.read_excel(file)
-                                    self.start_row = processFile(df)
-
+                                self.start_row = self.processFile(file)
 
 
 
-                                except Exception as e:
-                                    st.error(f"Error reading {file_name}: {str(e)}")
-                        else:
-                            st.warning(f"Skipping non-Excel file: {file_name}")
-            except zipfile.BadZipFile:
-                st.error("Archivo invalido.")
-            except Exception as e:
-                st.error(f"Error procesando zip: {str(e)}")
 
-    def processFile(file):
-        pass
+                            except Exception as e:
+                                st.error(f"Error reading {file_name}: {str(e)}")
+                    else:
+                        st.warning(f"Skipping non-Excel file: {file_name}")
+        except zipfile.BadZipFile:
+            st.error("Archivo invalido.")
+        except Exception as e:
+            st.error(f"Error procesando zip: {str(e)}")
+
+    def processFile(self, file):
+        input_data = pd.read_excel(file, header=19)  # header is in the 20th row (0-indexed)
+        wb = openpyxl.load_workbook(file)
+
